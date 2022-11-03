eb92fdbf3e5590eac55a7b72b32221b39bf5402c
jaejune Yun <jjune0427@naver.com>
2022-11-04 00:20:53 +0900

    Constructor 공부 및 TArray 사용

--- Source/Crypt_Raider/TriggerComponent.cpp
+++ Source/Crypt_Raider/TriggerComponent.cpp
@@ -3,7 +3,26 @@
 
 #include "TriggerComponent.h"
 
+UTriggerComponent::UTriggerComponent()													//라이브 코딩에 바로 적용되지 않아서 에디터를 닫고 빌드를 한 후 에디터를 다시 열어야한다.
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);		//Super이기 때문에 Parent의 Tick을 부른다. Parent는 h파일에서 확인할 수 있고 여기선 UBoxComponent이다.
+
+	TArray<AActor*> Actors;				//Actor*를 담는 Array 생성 Vector처럼 크기가 정해져있지 않은 Array이다.
+	GetOverlappingActors(Actors);		//Tick에 따라 여러 개의 Actor가 Overlapping 될 수 있기 때문에 TArray를 받는거 같음
+
+	if(Actors.Num() > 0){
+		FString name = Actors[0]->GetActorNameOrLabel();
+		UE_LOG(LogTemp, Display, TEXT("Actor[0]'s name: %s"), *name);
+	}
+
 }
--- Source/Crypt_Raider/TriggerComponent.h
+++ Source/Crypt_Raider/TriggerComponent.h
@@ -13,9 +13,14 @@ UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CRYPT_RAIDER_API UTriggerComponent : public UBoxComponent
 {
 	GENERATED_BODY()
+public:
+	UTriggerComponent();
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-	
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 };
